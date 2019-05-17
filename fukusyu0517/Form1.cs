902e8fa2c7338b177174933215c0ca8adcf9b58f
kai-machida <49702001+kai-machida@users.noreply.github.com>
2019-05-17 13:32:46 +0900

    未完成、作成中

--- fukusyu0517/Form1.cs
+++ fukusyu0517/Form1.cs
@@ -16,5 +16,11 @@ namespace fukusyu0517
         {
             InitializeComponent();
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += int.Parse(textBox1.Text);
+            label1.Top += int.Parse(textBox2.Text);
+        }
     }
 }
